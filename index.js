40eb6ebbbf26ab8fb05c650931921cd6aaccaadc
hsknutson <hsknutson@crimson.ua.edu>
2018-10-08 18:19:53 -0500

    added firebase functionality

---
+++ index.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+// TODO: Replace with your project's customized code snippet
+var config = {
+	apiKey: "AIzaSyBKNPd_5ZdZiOTKzm75KtOH8aGzNrtFECg",
+	authDomain: "battle-connect.firebaseapp.com",
+	databaseURL: "https://battle-connect.firebaseio.com/",
+	projectId: "battle-connect",
+};
+firebase.initializeApp(config);
+
+generateSensorData() {
+	
+}
