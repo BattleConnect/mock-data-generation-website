180680351dcae0c3153286903f951f7cdd9f6f63
hsknutson <hsknutson@crimson.ua.edu>
2018-10-08 17:57:08 -0500

    attempting fix for heroku

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
+var path = require('path');
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}/`);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}/`);
 });
