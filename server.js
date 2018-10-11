2ad1bc78371de5821b149ec4c9ff8680e44751dd
hsknutson <hsknutson@crimson.ua.edu>
2018-10-11 10:34:56 -0500

    testing out adding data to firebase

--- server.js
+++ server.js
@@ -4,10 +4,36 @@ var path = require('path');
 
 const PORT = process.env.PORT || 5000;
 
+app.use(express.bodyParser());
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.send(200);
+  generateSensorData();
 });
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}/`);
 });
+
+
+// Initialize Firebase
+// TODO: Replace with your project's customized code snippet
+var config = {
+	apiKey: "AIzaSyBKNPd_5ZdZiOTKzm75KtOH8aGzNrtFECg",
+	authDomain: "battle-connect.firebaseapp.com",
+	databaseURL: "https://battle-connect.firebaseio.com/",
+	projectId: "battle-connect",
+};
+firebase.initializeApp(config);
+var database = firebase.database();
+
+function generateSensorData() {
+  database().ref('devices/' + 456).set({
+    sensor_type: "vibration"
+  });
+}
